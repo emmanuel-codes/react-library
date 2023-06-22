44b04b6aff16b1d088e4492f48b0ce6beb224a57
Emmanuel Abraham Herrera <86703262+emmanuel-codes@users.noreply.github.com>
2023-06-21 23:13:53 -0700

    Add files via upload
    github said the folder was too big to upload so here is the src code only

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+//1.27.55
+
+export default App;
